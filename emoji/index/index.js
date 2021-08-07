4a99ca9195a0fe7e7d257af744690ded65d48944
pdd11997110103 <993080086@qq.com>
2021-08-07 17:14:43 +0800

    显示表情；

--- emoji/index/index.js
+++ emoji/index/index.js
@@ -1,5 +1,13 @@
 const app = getApp()
-
+const emojiMap = new Map([
+  ['1', '[微笑]'],
+  ['2', '[撇嘴]']
+])
 Page({
-  data: {}
+  data: {},
+  onShow(ev) {
+    console.log(emojiMap)
+    console.log(emojiMap.size)
+    console.log(emojiMap.values())
+  }
 })
